9c88f7594792c59a120fb29ad2feb5747bf0480c
Paul <paul@houldridge.com>
2016-09-14 09:54:01 -0700

    initial commit, empty Spring Boot application.

---
+++ src/main/java/com/northbender/CodeCaptainWebApplication.java
@@ -0,0 +1,12 @@
+package com.northbender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeCaptainWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeCaptainWebApplication.class, args);
+    }
+}
