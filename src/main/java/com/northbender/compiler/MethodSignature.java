bcbe4d2d71d9209fb414add7282ce339b6566a6a
Paul <paul@houldridge.com>
2016-10-20 16:57:34 -0700

    added test cases and implemented then MethodSignature class.

--- src/main/java/com/northbender/compiler/MethodSignature.java
+++ src/main/java/com/northbender/compiler/MethodSignature.java
@@ -1,11 +1,52 @@
 package com.northbender.compiler;
 
+import java.util.ArrayList;
+
+import org.springframework.util.ClassUtils;
+
 public class MethodSignature {
 	private String name;
 	private Class<?> returnType;
 	private Class<?>[] parameterTypes;
 	
 	public MethodSignature(String signature) {
+		String[] header = signature.replaceAll("\\(.*?\\)","").split(" ");
+		String[] parms = signature.substring(signature.indexOf("(")+1,signature.indexOf(")")).split(",\\s");
+
+		setName( header[header.length-1]);
+
+		try {
+			setReturnType( ClassUtils.forName(header[header.length-2],null));
+		} catch (ClassNotFoundException e) {
+			setReturnType( null);
+		}
+
+		ArrayList<Class<?>> parmTypes = new ArrayList<Class<?>>(); 
+		try {
+			for(String parm: parms) {
+				if( !parm.isEmpty()) {
+					String parmType = parm.substring(0,parm.indexOf(" "));
+					parmTypes.add(ClassUtils.forName(parmType,null));
+				}
+			}
+
+			Class<?>[] types = new Class<?>[parmTypes.size()];
+			setParameterTypes( parmTypes.toArray(types));
+		} catch (ClassNotFoundException e) {
+			setParameterTypes( null);
+		}
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setReturnType(Class<?> returnType) {
+		this.returnType = returnType;
+	}
+
+	public void setParameterTypes(Class<?>[] parameterTypes) {
+		this.parameterTypes = parameterTypes;
 	}
 
 	public String getName() {
--- src/test/java/com/northbender/compiler/MethodSignatureTest.java
+++ src/test/java/com/northbender/compiler/MethodSignatureTest.java
@@ -4,24 +4,38 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 public class MethodSignatureTest {
-	private String testSignature = "public static int addNumbers(int a, int b)";
+	private String testSignature = "private static int someMethod(java.lang.String hello, float b, boolean isSomething)";
+	private String testHelloSignature = "public void sayHello()";
 	
 	@Test
 	public void methodNameExtractedFromSignaturePassedToConstructor() {
 		MethodSignature signature = new MethodSignature(testSignature);
-		assertEquals("addNumbers", signature.getName());
+		assertEquals("someMethod", signature.getName());
 	}
 	
 	@Test
 	public void returntypeExtractedFromSignaturePassedToConstructor() {
 		MethodSignature signature = new MethodSignature(testSignature);
-		assertEquals(Integer.class, signature.getReturnType());
+		assertEquals(int.class, signature.getReturnType());
 	}
 
 	@Test
 	public void parametersExtractedFromSignaturePassedToConstructor() {
-		Class<?>[] expected = {Integer.class, Integer.class};
+		Class<?>[] expected = {String.class, float.class, boolean.class};
 		MethodSignature signature = new MethodSignature(testSignature);
+		assertArrayEquals(expected, signature.getParameterTypes());
+	}
+
+	@Test
+	public void voidReturntypeExtractedFromSignaturePassedToConstructor() {
+		MethodSignature signature = new MethodSignature(testHelloSignature);
+		assertEquals(void.class, signature.getReturnType());
+	}
+
+	@Test
+	public void emptyParametersExtractedFromSignaturePassedToConstructor() {
+		Class<?>[] expected = {};
+		MethodSignature signature = new MethodSignature(testHelloSignature);
 		assertArrayEquals(expected, signature.getParameterTypes());
 	}
 }
